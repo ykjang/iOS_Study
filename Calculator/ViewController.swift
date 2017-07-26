834db8f3355bc222228449b57721008b36b85127
yg.jang <yg.jang@isecommerce.co.kr>
2017-07-26 12:16:13 +0900

    최초 등록

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,17 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
